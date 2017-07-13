fa08999b119489ed2242efaf57c72b6b13498a8c
microwarriors <shrabani.majumdar@capgemini.com>
2017-07-13 17:32:39 +0530

    updated UI
    updated UI

---
+++ ldv-poc/backend/src/main/java/com/ge/controller/ListController.java
@@ -0,0 +1,23 @@
+package com.ge.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ListController {
+
+	private static final List<String> COURSES = Arrays.asList("Angularjs", "Angular", "Reactjs", "Emberjs", "Vuejs");
+
+	//@CrossOrigin(origins = "http://localhost:4200")
+	@RequestMapping(value = "/api/ldv", method = RequestMethod.GET)
+	public List<String> handleCoursesList() {
+		System.out.println("it' works!!");
+
+		return COURSES;
+	}
+}
